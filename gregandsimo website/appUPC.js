5e03380631e06a5af6d24714e7b31f75d84fed3b
bytniak <bytniewski.grzegorz@gmail.com>
2017-01-25 11:41:51 +0100

    test
    test

--- gregandsimo website/appUPC.js
+++ gregandsimo website/appUPC.js
@@ -10,6 +10,7 @@ var account;
 //data of the contract
 var abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"standard","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"decimalUnits","type":"uint8"},{"name":"tokenSymbol","type":"string"}],"payable":false,"type":"constructor"},{"payable":false,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}];
 var contractAddress = "0x5c62b492ae99db9c2a174f3328ddd2f98773291a"
+var mychanges = ""
 
 
 /* gives the updates/messages on the main screen */
@@ -71,12 +72,12 @@ window.onload = function() {
             // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
         web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545")); //use local node if any
     }
-	
+
 	//variables definition
     var UPCoinInst = web3.eth.contract(abi);
     var UPCoin = UPCoinInst.at(contractAddress);
-	
-	//check if there are accounts 
+
+	//check if there are accounts
     web3.eth.getAccounts(function(err, accs) {
         if (err != null) {
             alert("There was an error fetching your accounts.");
@@ -90,14 +91,14 @@ window.onload = function() {
 
         accounts = accs;
         account = accounts[0];
-	
+
 	//refresh balance
     var refreshBalanceInterval = setInterval(function() {
       refreshBalance();
     }, 1000); //1000=1s
 
     });
-	
+
 	//refresh accunts (if you switched between)
     var accountInterval = setInterval(function() {
       if (web3.eth.accounts[0] !== account) {
@@ -105,5 +106,5 @@ window.onload = function() {
         refreshBalance();
       }
     }, 1000); //1000=1s
-    
+
 }
